46375c26e476bd95cba6e5bf2bbe2b17bcc4e89b
Shawn Garbett <shawn.garbett@vanderbilt.edu>
2018-11-12 11:15:23 -0600

    Ran Tao comparison setup to Schildcrout's paper

--- setup.R
+++ setup.R
@@ -7,9 +7,9 @@ prev.grp             <- c(0.3,
                           0.3,
                           0.3)
 n.imp                <- 50
-p.central            <- c(0.8,
+p.central            <- c(0.8, # 10th to 90th percentile
                           0.8,
-                          0.8,
+                          0.6, # 20th to 80th percentile
                           0.8)
 conf.param.sim       <- rbind( c(-0.25, 0.5),
                                c(-0.25, 0.5),
@@ -19,13 +19,13 @@ ni.sim               <- rbind(c(4, 6),
                               c(4, 6), 
                               c(4, 6), 
                               c(4, 6))
-inits.sim            <- rbind(c(75, -1, -.5, -6, -.5, log(9),log(1.25),0,log(3.5)),
-                              c(75, -1, -.5, -6, -.5, log(9),log(1.25),0,log(3.5)),
-                              c(75, -1, -.5, -6, -.5, log(9),log(1.25),0,log(3.5)),
-                              c(75, -1, -.5, -6, -.5, log(9),log(1.25),0,log(3.5)))
+inits.sim            <- rbind(c(75, -0.5, -0.5, -1, -2, log(9),log(1.25),0,log(3.5)),
+                              c(75, -0.5, -0.5, -1, -6, log(9),log(1.25),0,log(3.5)),
+                              c(75, -0.5, -0.5, -1, -2, log(9),log(1.25),0,log(3.5)),
+                              c(75, -0.5, -0.5, -1, -2, log(9),log(1.25),0,log(3.5)))
 NsPerStratumUniv.sim <- matrix( c(150,100,150,
                                   150,100,150,
                                   150,100,150,
-                                  150,100,150), ncol=3, byrow=TRUE)
+                                  175, 50,175), ncol=3, byrow=TRUE)
 # Control TwoPhase RanTao
-hn_scale             <- c(1.5, 2, 0.5, 0.1)
+hn_scale             <- rep(2, 4)
