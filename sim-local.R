340aa9f603bd46cbe7c0524965396ce470f955e9
Shawn Garbett <shawn.garbett@vanderbilt.edu>
2019-08-26 14:54:46 -0500

    Putting sim-local back to recommended

--- sim-local.R
+++ sim-local.R
@@ -5,10 +5,10 @@ source("run.R")
 ## For exection on local desktop
 library(parallel)
  
-# mclapply(1:24, mc.cores=8, function(x)
-# {
-#   set.seed(x)
-#   sapply(1:n, function(y) simulation(x, y))
-# })
+mclapply(1:24, mc.cores=8, function(x)
+{
+  set.seed(x)
+  sapply(1:n, function(y) simulation(x, y))
+})
 
-mclapply(1:n, mc.cores=8, function(x) {set.seed(1); simulation(1, x)})
+#mclapply(1:n, mc.cores=8, function(x) {set.seed(1); simulation(1, x)})
